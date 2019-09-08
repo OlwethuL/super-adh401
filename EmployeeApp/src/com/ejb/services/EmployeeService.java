dc5ac1c7233313f146931cf1e1c6f40ada3e7e78
OlwethuL <54271184+OlwethuL@users.noreply.github.com>
2019-09-09 00:09:22 +0200

    Another try

---
+++ EmployeeApp/src/com/ejb/services/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.ejb.services;
+
+import com.jpa.entities.Employee;
+
+public interface EmployeeService {
+
+	public void addEmployee(Employee emp);
+	
+}package com.ejb.services;
+
+public interface EmployeeService {
+
+}
